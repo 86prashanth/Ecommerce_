0887986c0438e2543f1156e2da7f2a331158b76d
Ambala Prashanth <89448613+86prashanth@users.noreply.github.com>
2023-01-25 07:27:51 +0530

    first

--- Blog/urls.py
+++ Blog/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path('register/',authviews.register,name='register'),
     path('login/',authviews.loginpage,name='login'),
     path('logout/',authviews.logoutpage,name='logout'),
+    # path('password_reset/',authviews.PasswordResetView.as_view(),name='password_reset'),
+    # path('password_reset/done/',authviews.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    # path('reset/<uuidb64>/<token>/',authviews.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    # path('reset/done/',authviews.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
